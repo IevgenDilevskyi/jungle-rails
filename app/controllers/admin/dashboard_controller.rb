8a14c354397618e5482fc245a62989240961cc47
IevgenDilevskyi <e.dilevskiy@gmail.com>
2021-04-28 20:22:15 +0000

    add info to admin/dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_NAME'], password: ENV['ADMIN_PASS']
+
   def show
+    @products_count = Product.count
+    @categories_count = Category.count
   end
 end
